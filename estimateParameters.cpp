f35c768ca6689ec74dfa36fd1fa232b4b027d65c
Jacob Mortensen <jacob.w.mortensen@gmail.com>
2015-06-01 14:45:27 -0600

    Testing to figure out why lambda values are strange

--- estimateParameters.cpp
+++ estimateParameters.cpp
@@ -2,17 +2,38 @@
 using namespace Rcpp;
 // [[Rcpp::depends(RcppArmadillo)]]
 
-
-// [[Rcpp::export]]
-NumericMatrix CalcLogLambda(arma::mat H, arma::colvec lambdaStar, arma::colvec beta) {
-  return wrap(lambdaStar + H * beta - log(sum(exp(lambdaStar + H*beta))));
+// [[Rcpp::export]] 
+List amcmcUpdate(const arma::colvec& draw, const arma::colvec& curMn, const arma::mat& curVar, int curIt) {
+  arma::colvec mn;
+  arma::mat var;
+  if (curIt > 0) {
+    mn = ((curIt - 1) * curMn + draw) / curIt;
+    if (curIt == 1) {
+      var = arma::mat(draw.size(), draw.size(), arma::fill::zeros);
+    } else {
+      var = (curIt - 2) * curVar + (curIt - 1) * (curMn * curMn.t()) + draw * draw.t();
+      var = (var - curIt*(mn * mn.t())) / (curIt - 1);
+    }
+  } else {
+    mn = arma::colvec(curMn.size(), arma::fill::zeros);
+    var = arma::mat(draw.size(), draw.size(), arma::fill::zeros);
+  }
+  return List::create(Named("mn") = mn, Named("var") = var);
 }
 
+
 // [[Rcpp::export]]
-double LogLike(List H, arma::colvec lambdaStar, arma::colvec beta, NumericMatrix dateLocIndex) {
+NumericMatrix CalcLogLambda(const arma::mat& H, const arma::colvec& lambdaStar, const arma::colvec& beta) {
+  arma::colvec m = lambdaStar + H * beta;
+  return wrap(m - log(sum(exp(m))));
+}
+
+
+// [[Rcpp::export]]
+double LogLike(const List& H, const arma::colvec& lambdaStar, const arma::colvec& beta, const NumericMatrix& dateLocIndex) {
   NumericMatrix logLambdas(lambdaStar.size(), H.size());
   for (int i = 0; i < H.size(); i++) {
-    logLambdas(_, i) = CalcLogLambda(as<arma::mat>(H[i]), lambdaStar, beta);
+    logLambdas(_, i) = CalcLogLambda(H[i], lambdaStar, beta);
   }
   double out = 0;
   for (int i = 0; i < dateLocIndex.nrow(); i++) {
@@ -22,98 +43,100 @@ double LogLike(List H, arma::colvec lambdaStar, arma::colvec beta, NumericMatrix
 }
 
 // [[Rcpp::export]]
-double LogLambdaPrior(arma::colvec lambdaStar, double sig2, arma::mat lambdaInverseMatern) {
+double LogLambdaPrior(const arma::colvec& lambdaStar, const double& sig2, const arma::mat& lambdaInverseMatern) {
   return as_scalar(-0.5*(lambdaStar.t() * lambdaInverseMatern * lambdaStar) / sig2);
 }
 
 // [[Rcpp::export]]
-double LogBetaPrior(arma::colvec beta, double sig2, arma::mat betaInverseMatern) {
+double LogBetaPrior(const arma::colvec& beta, double sig2, const arma::mat& betaInverseMatern) {
   return as_scalar(-0.5*(beta.t() * betaInverseMatern * beta) / sig2);
 }
 
 // [[Rcpp::export]]
-NumericVector mvrnormC(int n, arma::vec mu, arma::mat sigma) {
+NumericVector mvrnormC(int n, const arma::vec& mu, const arma::mat& sigma) {
   int ncols = sigma.n_cols;
   arma::mat Y = arma::randn(n, ncols);
   return wrap(arma::repmat(mu, 1, n).t() + Y * arma::chol(sigma));
 }
 
 // [[Rcpp::export]]
-arma::colvec proposeLambda(arma::colvec currLambda, IntegerVector ptsIndex, arma::mat propVar) {
+void proposeLambda(arma::colvec& propLambda, const arma::colvec& currLambda, const IntegerVector& ptsIndex, const arma::mat& propVar) {
   NumericVector nvLam = wrap(currLambda);
   nvLam[ptsIndex-1] = mvrnormC(1, as<arma::vec>(nvLam[ptsIndex-1]), propVar);
-  return as<arma::colvec>(nvLam);
+  propLambda = as<arma::colvec>(nvLam);
 }
 
-// [[Rcpp::export]]
-List MHGibbsC(const int ndraws, const int thinFactor, const List TempData, const NumericVector initLambda, const NumericVector initBeta,
-              const arma::mat lambdaInverseMatern, const arma::mat betaInverseMatern, const NumericMatrix dateLocIndex,
-              const List closePtsIndex) {
-  // Vars to use throughout
-  int nPredLocs = initLambda.size();
-  int nBlocks = closePtsIndex.size();
-  int nLags = 4;
-  
-  // Initialize storage containers
-  NumericMatrix lambdaStar(ndraws, initLambda.size());
-  NumericMatrix beta(ndraws, initBeta.size());
-  lambdaStar(0, _) = initLambda;
-  beta(0, _) = initBeta;
-  
-  double logMH = 0;
-  
-  arma::colvec tempLambda = initLambda;
-  double tempLvar = 0.01;
-  double lvarA = 0.01;
-  double lvarB = 0.01;
-  double lamA = 0;
-  double lamB = 0;
-  double lambdaVarConst = 0.00000001;
-  NumericMatrix lambdaPropTemp = NumericMatrix(nPredLocs/nBlocks, nPredLocs/nBlocks);
-  lambdaPropTemp.fill_diag(lambdaVarConst);
-  arma::mat lambdaPropVar = as<arma::mat>(lambdaPropTemp);
-  
-  arma::colvec tempBeta = initBeta;
-  double tempBvar = 0.1;
-  double bvarA = 0.01;
-  double bvarB = 0.01;
-  double betaA = 0;
-  double betaB = 0;
-  double betaVarConst = 0.000005;
-  NumericMatrix betaPropTemp = NumericMatrix(nLags, nLags);
-  betaPropTemp.fill_diag(betaVarConst);
-  arma::mat betaPropVar = as<arma::mat>(betaPropTemp);  
-  
-  
-  int n = 0;
-  bool update = false;  
-  for (int i=1; i < ndraws*thinFactor; i++) {
-    update = (i % thinFactor == 0) ? true : false;
-    if (update) n += 1;
-    
-    lamA = lvarA + nPredLocs/2;
-    lamB = lvarB + as_scalar(0.5*tempLambda.t()*lambdaInverseMatern*tempLambda);
-    tempLvar = 1 / R::rgamma(lamA, lamB);
 
-    for (int j=0; j < nBlocks; j++) {
-      arma::colvec propLambda = proposeLambda(tempLambda, closePtsIndex[j], lambdaPropVar);
-      logMH = LogLike(TempData, propLambda, tempBeta, dateLocIndex) - LogLike(TempData, tempLambda, tempBeta, dateLocIndex);
-      logMH += LogLambdaPrior(propLambda, tempLvar, lambdaInverseMatern) - LogLambdaPrior(tempLambda, tempLvar, lambdaInverseMatern);
-      
-      tempLambda = (log(runif(1))[0] < logMH) ? propLambda : tempLambda;
-    }
-    if (update) lambdaStar(n, _) = as<NumericVector>(wrap(tempLambda));
-    
-    betaA = bvarA + nLags / 2;
-    betaB = bvarB + as_scalar(0.5*tempBeta.t()*betaInverseMatern*tempBeta);
-    tempBvar = 1/R::rgamma(betaA, betaB);
-    
-    arma::colvec propBeta = as<arma::colvec>(mvrnormC(1, tempBeta, betaPropVar));
-    logMH = LogLike(TempData, tempLambda, propBeta, dateLocIndex) - LogLike(TempData, tempLambda, tempBeta, dateLocIndex);
-    logMH += LogBetaPrior(propBeta, tempBvar, betaInverseMatern) - LogBetaPrior(tempBeta, tempBvar, betaInverseMatern);
-    
-    tempBeta = (log(runif(1))[0] < logMH) ? propBeta : tempBeta;
-    if (update) beta(n, _) = as<NumericVector>(wrap(tempBeta));
-  }
-  return List::create(Named("lambda.star") = lambdaStar, Named("beta") = beta);
-}
+//List MHGibbsC(const int ndraws, const int thinFactor, const List& TempData, const NumericVector& initLambda, const NumericVector& initBeta,
+//              const arma::mat& lambdaInverseMatern, const arma::mat& betaInverseMatern, const NumericMatrix& dateLocIndex,
+//              const List& closePtsIndex) {
+//  // Vars to use throughout
+//  int nPredLocs = initLambda.size();
+//  int nBlocks = closePtsIndex.size();
+//  int nLags = 4;
+//  
+//  // Initialize storage containers
+//  NumericMatrix lambdaStar(ndraws, initLambda.size());
+//  NumericMatrix beta(ndraws, initBeta.size());
+//  lambdaStar(0, _) = initLambda;
+//  beta(0, _) = initBeta;
+//  
+//  double logMH = 0;
+//  
+//  // Lambda stuff
+//  arma::colvec tempLambda = initLambda;
+//  arma::colvec propLambda;
+//  double tempLvar = 0.01;
+//  double lvarA = 0.01;
+//  double lvarB = 0.01;
+//  double lamA = 0;
+//  double lamB = 0;
+//  double lambdaVarConst = 0.01;
+//  arma::mat lambdaDiag = arma::mat(nPredLocs / nBlocks, nPredLocs / nBlocks, arma::fill::eye);
+//  arma::mat lambdaPropVar = lambdaVarConst * lambdaDiag;
+////  int lAMCMCIt = 500;
+//  
+//  // Beta stuff
+//  arma::colvec tempBeta = initBeta;
+//  arma::colvec propBeta;
+//  double tempBvar = 0.1;
+//  double bvarA = 0.01;
+//  double bvarB = 0.01;
+//  double betaA = 0;
+//  double betaB = 0;
+//  double betaVarConst = 0.00001;
+//  arma::mat betaPropVar = betaVarConst * arma::mat(nLags, nLags, arma::fill::eye);  
+//  
+//  int n = 0;
+//  bool update = false;  
+//  for (int i=1; i < ndraws*thinFactor; i++) {
+//    update = (i % thinFactor == 0) ? true : false;
+//    if (update) n += 1;
+//    
+//    lamA = lvarA + nPredLocs/2;
+//    lamB = lvarB + as_scalar(0.5*tempLambda.t()*lambdaInverseMatern*tempLambda);
+//    tempLvar = 1 / R::rgamma(lamA, lamB);
+//
+//    for (int j=0; j < nBlocks; j++) {
+////      if (i > lAMCMCIt) lambdaPropVar = (2.4 * 2.4 / (nPredLocs / nBlocks)) 
+//      proposeLambda(propLambda, tempLambda, closePtsIndex[j], lambdaPropVar);
+//      logMH = LogLike(TempData, propLambda, tempBeta, dateLocIndex) - LogLike(TempData, tempLambda, tempBeta, dateLocIndex);
+//      logMH += LogLambdaPrior(propLambda, tempLvar, lambdaInverseMatern) - LogLambdaPrior(tempLambda, tempLvar, lambdaInverseMatern);
+//      
+//      tempLambda = (log(runif(1))[0] < logMH) ? propLambda : tempLambda;
+//    }
+//    if (update) lambdaStar(n, _) = as<NumericVector>(wrap(tempLambda));
+//    
+//    betaA = bvarA + nLags / 2;
+//    betaB = bvarB + as_scalar(0.5*tempBeta.t()*betaInverseMatern*tempBeta);
+//    tempBvar = 1/R::rgamma(betaA, betaB);
+//    
+//    propBeta = as<arma::colvec>(mvrnormC(1, tempBeta, betaPropVar));
+//    logMH = LogLike(TempData, tempLambda, propBeta, dateLocIndex) - LogLike(TempData, tempLambda, tempBeta, dateLocIndex);
+//    logMH += LogBetaPrior(propBeta, tempBvar, betaInverseMatern) - LogBetaPrior(tempBeta, tempBvar, betaInverseMatern);
+//    
+//    tempBeta = (log(runif(1))[0] < logMH) ? propBeta : tempBeta;
+//    if (update) beta(n, _) = as<NumericVector>(wrap(tempBeta));
+//  }
+//  return List::create(Named("lambda.star") = lambdaStar, Named("beta") = beta);
+//}
--- estimateParameters_lambda_only.R
+++ estimateParameters_lambda_only.R
@@ -101,7 +101,7 @@ sourceCpp("./estimateParameters.cpp")
 
 # Metropolis within Gibbs sampler to estimate lambda and beta coefficients
 # Gibbs sampler to estimate sigma2 and lambda
-MHGibbs <- function(ndraws, thin.factor, init.lambda, init.beta) {
+MHGibbs.lambda.only <- function(ndraws, thin.factor, init.lambda, init.beta) {
   # define variables to use throughout mh.gibbs
   vars <- c("HI_MAX","HI_MIN","T2MAX","T2MIN","SW_MIN","SW_MAX")
   postfix <- c(".0",".1",".2",".3")
@@ -113,19 +113,19 @@ MHGibbs <- function(ndraws, thin.factor, init.lambda, init.beta) {
   
   # initialize containers to hold drawsta
   lambda.star <- matrix(NA, nrow=ndraws, ncol=num.pred.locs)
-  beta <- matrix(NA, nrow=ndraws, ncol=num.lags)
+  beta <- matrix(0, nrow=ndraws, ncol=num.lags)
   
   # create initial proposal and initialize variables 
   lambda.star[1, ] <- init.lambda
-  beta[1, ] <- init.beta
+#   beta[1, ] <- init.beta
   temp.lstar <- init.lambda
   temp.lvar <- 0.01
   lambda.var.a <- 0.01
   lambda.var.b <- 0.01
-  temp.beta <- init.beta
-  temp.bvar <- 0.1
-  beta.var.a <- 0.01
-  beta.var.b <- 0.01
+  temp.beta <- c(0,0,0,0)
+#   temp.bvar <- 0.1
+#   beta.var.a <- 0.01
+#   beta.var.b <- 0.01
   
 
   # delta is conjugate so we just draw it up front
@@ -186,132 +186,6 @@ MHGibbs <- function(ndraws, thin.factor, init.lambda, init.beta) {
       if (log(runif(1)) < log.MH) {
         new.lstar[close.pts.index[[j]]] <- prop.lstar
       }
-      new.amcmc <- AMCMC.update(new.lstar[close.pts.index[[j]]], 
-                                amcmc[[j]]$mn, amcmc[[j]]$var, i-1)
-      amcmc[[j]] <- new.amcmc
-    }
-    temp.lstar <- new.lstar
-    if (update) lambda.star[n.draw, ] <- temp.lstar
-    
-    # Get draws for beta.var using the complete conditional
-    beta.a <- beta.var.a + num.lags/2
-    beta.b <- beta.var.b + (1/2)*t(temp.beta)%*%
-      beta.inverse.matern%*%(temp.beta)
-    temp.bvar <- 1/rgamma(1, shape=beta.a, rate=beta.b)
-    
-    # Metropolis hastings to get draws for beta
-    prop.var.const <- 1e-5
-    if (i < beta.amcmc.it) {
-      prop.var <- prop.var.const*diag(num.lags)
-    } else {
-      prop.var <-  (2.4^2/num.lags)*
-        (prop.var.const*diag(num.lags)+beta.amcmc$var)
-    }
-    prop.beta <- mvrnorm(1, temp.beta, prop.var)
-    log.MH <- LogLike(H, temp.lstar, prop.beta, merged.mat) - LogLike(H, temp.lstar, temp.beta, merged.mat)
-    log.MH <- log.MH + LogBetaPrior(prop.beta, temp.bvar, beta.inverse.matern) -
-      LogBetaPrior(temp.beta, temp.bvar, beta.inverse.matern)
-    temp.beta <- if(log(runif(1)) < log.MH) prop.beta else temp.beta
-    if (update) beta[n.draw, ] <- temp.beta
-
-    new.amcmc <- AMCMC.update(temp.beta, 
-                              beta.amcmc$mn, beta.amcmc$var, i-1)
-    beta.amcmc <- new.amcmc
-
-    setTxtProgressBar(pb, i)
-  }
- 
-  return(list(delta=delta, lambda.star=lambda.star, beta=beta, race=race.draws, gender=gender.draws, age=age.draws))
-}
-
-
-# Metropolis within Gibbs sampler to estimate lambda and beta coefficients
-# Gibbs sampler to estimate sigma2 and lambda
-MHGibbs2 <- function(ndraws, thin.factor, init.lambda, init.beta) {
-  # define variables to use throughout mh.gibbs
-  vars <- c("HI_MAX","HI_MIN","T2MAX","T2MIN","SW_MIN","SW_MAX")
-  postfix <- c(".0",".1",".2",".3")
-  # for now, this only evaluates for HI_MAX
-  lagged.vars <- paste(vars[1], postfix, sep="")
-  H <- lapply(temp.data.nomiss, function(x) { as.matrix(x[,lagged.vars]) })
-  pb <- txtProgressBar(min=0, max=ndraws*thin.factor, style=3)
-  n.draw <- ifelse(thin.factor == 1, 1, 0)
-  
-  # initialize containers to hold drawsta
-  lambda.star <- matrix(NA, nrow=ndraws, ncol=num.pred.locs)
-  beta <- matrix(NA, nrow=ndraws, ncol=num.lags)
-  
-  # create initial proposal and initialize variables 
-  lambda.star[1, ] <- init.lambda
-  beta[1, ] <- init.beta
-  temp.lstar <- init.lambda
-  temp.lvar <- 0.01
-  lambda.var.a <- 0.01
-  lambda.var.b <- 0.01
-  temp.beta <- init.beta
-  temp.bvar <- 0.1
-  beta.var.a <- 0.01
-  beta.var.b <- 0.01
-  
-  
-  # delta is conjugate so we just draw it up front
-  delta <- rgamma(ndraws, shape=nrow(calls)+0.001, rate=1.001)
-  
-  # fit age with a dirichlet prior, which is conjugate
-  age.alpha <- 2
-  count.ages <- data.frame(table(calls$Age))
-  names(count.ages) <- c("age", "count")
-  count.ages$age <- FacToNum(count.ages$age)
-  count.ages$count <- FacToNum(count.ages$count)
-  n.ages <- merge(data.frame(age=0:100), count.ages, all.x=TRUE)
-  n.ages$count[is.na(n.ages$count)] <- 0
-  age.draws <- rdirichlet(ndraws, n.ages$count + age.alpha)
-  
-  # fit gender with a dirichlet prior
-  gender.alpha <- 2
-  n.gender <- data.frame(table(calls$Gender))
-  names(n.gender) <- c("gender", "count")
-  gender.draws <- rdirichlet(ndraws, n.gender$count + gender.alpha)
-  
-  # fit race with a dirichlet prior
-  race.alpha <- 2
-  n.race <- data.frame(table(calls$Eth))
-  names(n.race) <- c("race", "count")
-  race.draws <- rdirichlet(ndraws, n.race$count + race.alpha)
-  
-  for (i in 2:(ndraws*thin.factor)) {
-    update <- ifelse(i %% thin.factor == 0, TRUE, FALSE)
-    if (update) n.draw <- n.draw + 1
-    
-    # Get draws for lambda.var using the complete conditional
-    lambda.a <- lambda.var.a + num.pred.locs/2
-    lambda.b <- lambda.var.b + (1/2)*t(temp.lstar)%*%
-      lambda.inverse.matern%*%(temp.lstar) 
-    temp.lvar <- 1/rgamma(1, shape=lambda.a, rate=lambda.b)
-    
-    # Here we implement metropolis hastings to get draws for lambda
-    new.lstar <- temp.lstar
-    for (j in 1:num.blocks) {
-      # If enough iterations have been completed, begin using adaptive MCMC techniques
-      prop.var.const <- 1e-2
-      if (i < amcmc.it) {
-        prop.var <- prop.var.const*diag(num.pred.locs/num.blocks)
-      } else {
-        prop.var <- (2.4^2/(num.pred.locs/num.blocks))*
-          (prop.var.const*diag(num.pred.locs/num.blocks)+amcmc[[j]]$var)
-      }
-      
-      prop.lstar <- mvrnorm(1, new.lstar[close.pts.index[[j]]], prop.var) 
-      prop.lstar.vec <- new.lstar
-      prop.lstar.vec[close.pts.index[[j]]] <- prop.lstar
-      
-      log.MH <- LogLike2(H, prop.lstar.vec, temp.beta, merged.mat) - LogLike2(H, new.lstar, temp.beta, merged.mat) 
-      log.MH <- log.MH + LogLambdaPrior(prop.lstar.vec, temp.lvar, lambda.inverse.matern) - 
-        LogLambdaPrior(new.lstar, temp.lvar, lambda.inverse.matern)
-      
-      if (log(runif(1)) < log.MH) {
-        new.lstar[close.pts.index[[j]]] <- prop.lstar
-      }
       new.amcmc <- amcmcUpdate(new.lstar[close.pts.index[[j]]], 
                                 amcmc[[j]]$mn, amcmc[[j]]$var, i-1)
       amcmc[[j]] <- new.amcmc
@@ -320,45 +194,42 @@ MHGibbs2 <- function(ndraws, thin.factor, init.lambda, init.beta) {
     if (update) lambda.star[n.draw, ] <- temp.lstar
     
     # Get draws for beta.var using the complete conditional
-    beta.a <- beta.var.a + num.lags/2
-    beta.b <- beta.var.b + (1/2)*t(temp.beta)%*%
-      beta.inverse.matern%*%(temp.beta)
-    temp.bvar <- 1/rgamma(1, shape=beta.a, rate=beta.b)
-    
-    # Metropolis hastings to get draws for beta
-    prop.var.const <- 1e-5
-    if (i < beta.amcmc.it) {
-      prop.var <- prop.var.const*diag(num.lags)
-    } else {
-      prop.var <-  (2.4^2/num.lags)*
-        (prop.var.const*diag(num.lags)+beta.amcmc$var)
-    }
-    prop.beta <- mvrnorm(1, temp.beta, prop.var)
-    log.MH <- LogLike2(H, temp.lstar, prop.beta, merged.mat) - LogLike2(H, temp.lstar, temp.beta, merged.mat)
-    log.MH <- log.MH + LogBetaPrior(prop.beta, temp.bvar, beta.inverse.matern) -
-      LogBetaPrior(temp.beta, temp.bvar, beta.inverse.matern)
-    temp.beta <- if(log(runif(1)) < log.MH) prop.beta else temp.beta
-    if (update) beta[n.draw, ] <- temp.beta
-    
-    new.amcmc <- amcmcUpdate(temp.beta, 
-                              beta.amcmc$mn, beta.amcmc$var, i-1)
-    beta.amcmc <- new.amcmc
-    
+#     beta.a <- beta.var.a + num.lags/2
+#     beta.b <- beta.var.b + (1/2)*t(temp.beta)%*%
+#       beta.inverse.matern%*%(temp.beta)
+#     temp.bvar <- 1/rgamma(1, shape=beta.a, rate=beta.b)
+#     
+#     # Metropolis hastings to get draws for beta
+#     prop.var.const <- 1e-5
+#     if (i < beta.amcmc.it) {
+#       prop.var <- prop.var.const*diag(num.lags)
+#     } else {
+#       prop.var <-  (2.4^2/num.lags)*
+#         (prop.var.const*diag(num.lags)+beta.amcmc$var)
+#     }
+#     prop.beta <- mvrnorm(1, temp.beta, prop.var)
+#     log.MH <- LogLike(H, temp.lstar, prop.beta, merged.mat) - LogLike(H, temp.lstar, temp.beta, merged.mat)
+#     log.MH <- log.MH + LogBetaPrior(prop.beta, temp.bvar, beta.inverse.matern) -
+#       LogBetaPrior(temp.beta, temp.bvar, beta.inverse.matern)
+#     temp.beta <- if(log(runif(1)) < log.MH) prop.beta else temp.beta
+#     if (update) beta[n.draw, ] <- temp.beta
+# 
+#     new.amcmc <- AMCMC.update(temp.beta, 
+#                               beta.amcmc$mn, beta.amcmc$var, i-1)
+#     beta.amcmc <- new.amcmc
+
     setTxtProgressBar(pb, i)
   }
-  
+ 
   return(list(delta=delta, lambda.star=lambda.star, beta=beta, race=race.draws, gender=gender.draws, age=age.draws))
 }
 
-init.beta <- c(-0.15, 0.15, 0.08, 0)
 init.lambda <- rep(log(1/num.pred.locs), num.pred.locs)
 
 
-Rprof()
-t1 <- system.time(draws <- MHGibbs(10, 1, init.lambda, init.beta))
-t2 <- system.time(draws2 <- MHGibbs2(10, 1, init.lambda, init.beta))
-tm <- microbenchmark(MHGibbs(10, 1, init.lambda, init.beta), MHGibbs2(10, 1, init.lambda, init.beta), MHGibbs3(10, 1, init.lambda, init.beta), times=10)
+Rprof("lambdaOnly.out")
+time <- system.time(draws.lambda.only <- MHGibbs.lambda.only(1000, 1, init.lambda, init.beta))
 Rprof(NULL)
-summaryRprof()
+summaryRprof("lambdaOnly.out")
 # save(draws, file="./RData/MHDrawsHI_MAX.RData")
 
